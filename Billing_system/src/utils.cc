ab9cab9f1e29e241f825e2ee7f80f74bbb396530
IPG2004 <osteriz167@gmail.com>
2024-12-29 09:08:38 +0000

    Deleting unnecesesary files

--- Billing_system/src/utils.cc
+++
@@ -1,8 +0,0 @@
-// utils.cc
-#include "utils.h"
-
-std::string doubleToStringWithTwoDecimals(double value) {
-    std::ostringstream out;
-    out << std::fixed << std::setprecision(2) << value;
-    return out.str();
-}
